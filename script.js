09918f87ecd816cba7c6d29703885c5d6a3dd9c9
nvm03 <stasriznik150580@gmail.com>
2025-11-01 20:41:47 +0100

    small fixes

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ const email = document.getElementById("email")
 const inputError = document.getElementById("email-error")
 
 function showError(){
-    if(email.validity.valueMissing) {
-        inputError.textContent = "Valid email required"
-        email.classList.add('input-error')
-    } else if (email.validity.typeMismatch) {
+    if(email.validity.valueMissing || email.validity.typeMismatch) {
         inputError.textContent = "Valid email required"
         email.classList.add('input-error')
     }
 }
+function cleanError(){
+    inputError.textContent = ""
+    email.classList.remove('input-error')  
+}
 
-email.addEventListener("input", function(e){
+email.addEventListener("input", function(){
      if(!email.validity.valid){
         showError();
+     }else{
+        cleanError();
      }
  })
 
@@ -22,8 +25,6 @@ form.addEventListener("submit", function(e){
     if(!email.validity.valid){
         showError();
         e.preventDefault();
-    }else{
-        
     }
 })
 
