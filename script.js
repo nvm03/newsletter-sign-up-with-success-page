b37b651d76350ef28d0606b6f0589d7541c813cd
nvm03 <stasriznik150580@gmail.com>
2025-11-01 20:59:26 +0100

    small fixes

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const form = document.getElementById("form")
 const email = document.getElementById("email")
 const inputError = document.getElementById("email-error")
+const submitButton = document.getElementById("submit")
 
 function showError(){
     if(email.validity.valueMissing || email.validity.typeMismatch) {
         inputError.textContent = "Valid email required"
         email.classList.add('input-error')
+    }else{
+        inputError.textContent = ""
+        email.classList.remove('input-error')
     }
 }
 function cleanError(){
@@ -16,8 +20,10 @@ function cleanError(){
 email.addEventListener("input", function(){
      if(!email.validity.valid){
         showError();
+        submitButton.classList.remove('submit-btn-hover')
      }else{
         cleanError();
+        submitButton.classList.add('submit-btn-hover')
      }
  })
 
